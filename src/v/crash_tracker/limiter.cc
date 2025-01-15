e92afde5b69030b8d3eb59351c884382be9415f9
Gellért Peresztegi-Nagy <gellert.nagy@redpanda.com>
2025-01-15 16:04:29 +0000

    crash_tracker: define API
    Define the core API of the crash recording module ahead of development
    to help parallelise work by reducing dependencies and unblocking some
    tasks.
    
    Note though the dependencies between the components, the types used in
    the interfaces and the lifetimes of the objects. Specifically note that
    the `recorder` is defined as a static global singleton. This is to
    ensure that it can be used in signal handler functions which are defined
    as function pointers.

---
+++ src/v/crash_tracker/limiter.cc
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2025 Redpanda Data, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#include "crash_tracker/limiter.h"
+
+namespace crash_tracker {} // namespace crash_tracker
